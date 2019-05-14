28271e2ec185c2f1f584f08fa55c52beac2bc477
17010265 <GoodBushFD2R>
2019-05-14 20:45:44 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/carapp/DBHelper.java
@@ -0,0 +1,118 @@
+package sg.edu.rp.c346.carapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VER = 1;
+    // Filename of the database
+    private static final String DATABASE_NAME = "cars.db";
+
+    private static final String TABLE_TASK = "Car";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_BRAND = "brand";
+    private static final String COLUMN_LITRE = "litre";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableSql = "CREATE TABLE " + TABLE_TASK +  "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_BRAND + " TEXT,"
+                + COLUMN_LITRE + " REAL )";
+        db.execSQL(createTableSql);
+        Log.i("info" ,"created tables");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
+        // Create table(s) again
+        onCreate(db);
+    }
+
+    public void insertTask(String brand, double litre){
+
+        // Get an instance of the database for writing
+        SQLiteDatabase db = this.getWritableDatabase();
+        // We use ContentValues object to store the values for
+        //  the db operation
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_BRAND, brand);
+        values.put(COLUMN_LITRE, litre);
+        // Insert the row into the TABLE_TASK
+        db.insert(TABLE_TASK, null, values);
+        // Close the database connection
+        db.close();
+    }
+
+    public ArrayList<String> getTaskContent() {
+        // Create an ArrayList that holds String objects
+        ArrayList<String> tasks = new ArrayList<String>();
+        // Select all the tasks' description
+        String selectQuery = "SELECT " + COLUMN_BRAND
+                + " FROM " + TABLE_TASK;
+
+        // Get the instance of database to read
+        SQLiteDatabase db = this.getReadableDatabase();
+        // Run the SQL query and get back the Cursor object
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // moveToFirst() moves to first row
+        if (cursor.moveToFirst()) {
+            // Loop while moveToNext() points to next row
+            //  and returns true; moveToNext() returns false
+            //  when no more next row to move to
+            do {
+                // Add the task content to the ArrayList object
+                //  0 in getString(0) return the data in the first
+                //  column in the Cursor object. getString(1)
+                //  return second column data and so on.
+                //  Use getInt(0) if data is an int
+                tasks.add(cursor.getString(0));
+            } while (cursor.moveToNext());
+        }
+        // Close connection
+        cursor.close();
+        db.close();
+
+        return tasks;
+    }
+
+    public ArrayList<Car> getBrands() {
+        ArrayList<Car> cars = new ArrayList<Car>();
+        String selectQuery = "SELECT " + COLUMN_ID + ", "
+                + COLUMN_BRAND + ", "
+                + COLUMN_LITRE
+                + " FROM " + TABLE_TASK;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String brand = cursor.getString(1);
+                double litre = cursor.getDouble(2);
+                Car obj = new Car(id, brand, litre);
+                cars.add(obj);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return cars;
+    }
+
+
+}
---
+++ app/src/main/java/sg/edu/rp/c346/carapp/MainActivity.java
@@ -0,0 +1,67 @@
+package sg.edu.rp.c346.carapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert, btnRetrieve;
+    EditText etBrand, etLitre;
+    TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnInsert = findViewById(R.id.btnInsert);
+        btnRetrieve = findViewById(R.id.btnRetrieve);
+        etBrand = findViewById(R.id.etBrand);
+        etLitre = findViewById(R.id.etLitre);
+        tvResult = findViewById(R.id.tvResult);
+
+        btnInsert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                String brand = etBrand.getText().toString();
+                double litre = Double.parseDouble(etLitre.getText().toString());
+                // Insert a task
+                db.insertTask(brand, litre);
+                db.close();
+            }
+        });
+
+        btnRetrieve.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                ArrayList<String> data = db.getTaskContent();
+                db.close();
+
+                String txt = "";
+                for (int i = 0; i < data.size(); i++) {
+                    Log.d("Database Content", i +". "+data.get(i));
+                    txt += data.get(i) + "\n";
+                }
+
+                tvResult.setText(txt);
+            }
+        });
+
+    }
+}
